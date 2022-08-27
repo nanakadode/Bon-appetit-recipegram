0b4ab154306b36ecec334d331b7e039d903843c8
Nana kadode <amar.nana5048@gmail.com>
2022-08-27 21:42:16 +0900

    controller recipe create def

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,7 +12,7 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
-      redirect_to recipe_path
+      redirect_to recipe_path(@recipe)
     else
       render :new
     end
