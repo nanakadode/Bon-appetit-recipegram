4bfd98d2c1d95b538e1c646958eb43397bb999e8
Nana kadode <amar.nana5048@gmail.com>
2022-08-28 22:32:33 +0900

    Fixed photo attach tag

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,7 @@ class RecipesController < ApplicationController
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def new
@@ -12,7 +13,6 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user_id = current_user.id
-    raise
     if @recipe.save
       redirect_to recipe_path(@recipe)
     else
