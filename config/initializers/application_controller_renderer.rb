074342256b8482cc1ada9b9fd3d0a25d3d0551f3
Nana kadode <amar.nana5048@gmail.com>
2022-08-29 11:16:17 +0900

    Added initialier

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cb43fe2d7ac80bdd3df80819c6d7dd7234ee920dc5fdf2a0779b06c657e5c0be525f4ed79ec2ef851eb5e525c89e4a9cfe38a120802a9787705a3d75a1d4c78e'
