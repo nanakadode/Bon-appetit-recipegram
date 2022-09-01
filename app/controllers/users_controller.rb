40ebf527e84e3eb7e45e3d6ed70a60693626cd70
Nana kadode <amar.nana5048@gmail.com>
2022-09-01 16:28:39 +0900

    recipe destroy created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def index
     @users = User.all
     # @recipes = User.find([:id]).recipe
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   resources :users, only: %i[index show edit update]
-  resources :recipes, only: %i[index show new create edit update]
+  end
+
+  resources :recipes, only: %i[index show new create edit update destroy]
 end
