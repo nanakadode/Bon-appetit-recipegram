d572300cb534f77086d2fd9ea64213dd5442fa7c
Nana kadode <amar.nana5048@gmail.com>
2022-09-02 10:02:06 +0900

    Delete recipe button created

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   resources :users, only: %i[index show edit update]
-  end
 
-  resources :recipes, only: %i[index show new create edit update destroy]
+  resources :recipes, only: %i[index new create edit update show destroy]
 end
