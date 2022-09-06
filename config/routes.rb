3794b958deb73646571c0c0ab7691291109c5dcb
Nana kadode <amar.nana5048@gmail.com>
2022-09-06 17:58:08 +0900

    like routes nodified

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :users, only: %i[index show edit update] do
-  end
+  resources :users, only: %i[index show edit update]
 
   resources :recipes, only: %i[index new create edit update show destroy] do
     resources :like, only: %i[create destroy]
