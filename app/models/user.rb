af1f0ee70c00d5df5233e6f4998dbcdf8d6a5dd4
Nana kadode <amar.nana5048@gmail.com>
2022-09-06 15:17:50 +0900

    Error message for user edit page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
   attachment :profile_img
   has_many :recipes, dependent: :destroy
+
+  validates :username, presence: true
+
 end
