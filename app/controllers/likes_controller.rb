19306e6d68cb15957ca9dcacdefcab4d851e1131
Nana kadode <amar.nana5048@gmail.com>
2022-09-07 14:42:28 +0900

    created destroy def in like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,13 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(user_id: current_user.id, recipe_id: @recipe.id)
     @like.save
-    redirect_to recipes_path
+    redirect_to user_path
   end
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, recipe_id: @recipe.id)
     @like.destroy
+    redirect_to user_path
   end
 
   private
