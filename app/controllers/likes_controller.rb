b9e37639c069a137155a30ceeca688c07176647f
Nana kadode <amar.nana5048@gmail.com>
2022-09-08 16:43:12 +0900

    like toggle created

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,24 +1,24 @@
 class LikesController < ApplicationController
 
   def create
-    @like = Like.new(user_id: current_user.id, recipe_id: params[:id])
+    @like = Like.new(user_id: current_user.id, recipe_id: set_like.id)
     if @like.save
-      redirect_to recipes_path, success: 'Liked'
+      redirect_to recipe_path(set_like.id), success: 'Liked'
     else
-      redirect_to recipes_path, danger: 'Like failed'
+      redirect_to recipe_path(set_like.id), danger: 'Like failed'
     end
   end
 
   def destroy
-    @like = Like.find_by(user_id: current_user.id, recipe_id:  params[:id])
+    @like = Like.find_by(user_id: current_user.id, recipe_id: set_like.id)
     @like.destroy
-    redirect_to recipes_path
+    redirect_to recipe_path(set_like.id)
   end
 
   private
 
-  # def set_like
-  #   @recipe = Recipe.find(params[:recipe_id])
-  # end
+  def set_like
+    @recipe = Recipe.find(params[:recipe_id])
+  end
 
 end
