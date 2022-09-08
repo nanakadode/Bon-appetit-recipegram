16fe8adb93597fb2766ec7c1661c8385ee80720c
Nana kadode <amar.nana5048@gmail.com>
2022-09-08 17:06:58 +0900

    member like routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :users, only: %i[index show edit update]
+  resources :users, only: %i[index show edit update] do
+    member do
+      get :likes
+    end
+  end
 
   resources :recipes, only: %i[index new create edit update show destroy] do
     resource :likes, only: %i[create destroy]
